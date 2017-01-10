61af2bd7d867519b59a01893318fbcbc41a83be0
Andrew Gits <andrewgits@gmail.com>
2017-01-10 14:22:11 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# eslint-config-oneflow
+
+This package provides oneflow's .eslintrc as an extensible shared config.
+
+## Usage
+
+We've created the following configuration and rule sets:
+
+### eslint-config-oneflow
+
+Our default export contains all of our ESLint rules.
+
+1. `npm install --save-dev eslint-config-oneflow`
+2. add `"extends": "oneflow"` to your .eslintrc
---
+++ rules/default.js
@@ -0,0 +1,19 @@
+module.exports = {
+	env: {
+		es6: true,
+		node: true,
+		mocha: true
+	},
+	rules: {
+		'comma-dangle': [2, 'never'],
+		'arrow-parens': [2, 'as-needed'],
+		'padded-blocks': 0,
+		'indent': [1, 'tab'],
+		'no-tabs': 0,
+        'no-underscore-dangle': [2, { 'allow': ['_id', '__v'] } ],
+        'no-param-reassign': [2, { 'props': false }],
+        'object-shorthand': [2, 'never'],
+        'new-cap': [2, { 'capIsNewExceptions': ['Router', 'ObjectId'] }],
+		'linebreak-style': 0
+	}
+};
