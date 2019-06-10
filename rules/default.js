a16dcda4c9ae19bf42735d0423ca6402c2661afa
michaelfitzhavey <michael.fitzhavey@gmail.com>
2019-06-10 13:50:58 +0100

    chore: tidy rule order

--- rules/default.js
+++ rules/default.js
@@ -5,17 +5,18 @@ module.exports = {
 		mocha: true
 	},
 	rules: {
-		'comma-dangle': [2, 'never'],
 		'arrow-parens': [2, 'as-needed'],
-		'padded-blocks': 0,
+		'comma-dangle': [2, 'never'],
 		indent: [1, 'tab', { SwitchCase: 1 }],
-		'no-tabs': 0,
-		'no-underscore-dangle': [2, { allow: ['_id', '__v'] }],
-		'no-param-reassign': [2, { props: false }],
-		'object-shorthand': [2, 'never'],
-		'new-cap': [2, { capIsNewExceptions: ['Router', 'ObjectId'] }],
 		'linebreak-style': 0,
 		'max-len': [1, 120],
-		'no-plusplus': 0
+		'new-cap': [2, { capIsNewExceptions: ['Router', 'ObjectId'] }],
+		'no-param-reassign': [2, { props: false }],
+		'no-plusplus': 0,
+		'no-underscore-dangle': [2, { allow: ['_id', '__v'] }],
+		'no-tabs': 0,
+		'object-curly-newline': 0,
+		'object-shorthand': ['error', 'always'],
+		'padded-blocks': 0
 	}
 };
